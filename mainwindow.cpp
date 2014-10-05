974157b8d7dba1608550b1b50d6ef4bed03108a8
m-arch <themouallem@gmail.com>
2014-10-05 12:17:49 +0300

    text finder

---
+++ mainwindow.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QtCore/QFile>
+#include <QtCore/QTextStream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    loadTextFile();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_findButton_clicked()
+{
+     QString searchString = ui->lineEdit->text();
+     ui->textEdit->find(searchString, QTextDocument::FindWholeWords);
+}
+
+void MainWindow::loadTextFile()
+{
+    QFile inputFile(":/input.txt");
+    inputFile.open(QIODevice::ReadOnly);
+    QTextStream in(&inputFile);
+    QString line = in.readAll();
+    inputFile.close();
+
+    ui->textEdit->setPlainText(line);
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, 1);
+}
---
+++ pnote.pro
@@ -0,0 +1,26 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-10-05T11:45:13
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = pnote
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+OTHER_FILES +=
+
+RESOURCES += \
+    mainwindow.qrc
+
